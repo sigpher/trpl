108365b54ba88ecae66556f5ee61d9b477741e03
sigpher <425223341@qq.com>
2024-08-19 17:26:48 +0800

    finish chapter12->12.2

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
