32399c63198b54159d38109c8e4caef667575857
sigpher <425223341@qq.com>
2024-08-20 17:16:47 +0800

    finish chapter 13's Inventory demo

---
+++ ch13/src/main.rs
@@ -0,0 +1,25 @@
+use ch13::{
+    Inventory,
+    ShirtColor::{self, Blue, Red},
+};
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![Blue, Red, Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
