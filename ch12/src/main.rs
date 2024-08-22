206dadbc237d03c0823d5c97d143c83f8516f5df
sigpher <425223341@qq.com>
2024-08-22 10:14:58 +0800

    finish chapter ch13

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -3,16 +3,15 @@ use std::{env, process};
 use ch12::{run, Config};
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
+    // let args = env::args().collect::<Vec<String>>();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = run(config){
-       eprintln!("Application error: {e}");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
-
