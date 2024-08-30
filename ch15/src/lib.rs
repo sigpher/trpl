979a1bad404d01b49d6d53a313f7e1ebf39d7bbc
choi <38996146+sigpher@users.noreply.github.com>
2024-08-30 17:05:41 +0800

    chapter ch15

--- ch15/src/lib.rs
+++ ch15/src/lib.rs
@@ -1,8 +1,8 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc};
 
 #[derive(Debug)]
 pub enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -29,5 +29,47 @@ pub struct CustomSmartPointer {
 impl Drop for CustomSmartPointer {
     fn drop(&mut self) {
         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+pub fn hello(name: &str) {
+    println!("Hello {name}");
+}
+
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent Warming: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
     }
 }
--- ch15/src/main.rs
+++ ch15/src/main.rs
@@ -1,7 +1,7 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc};
 
 use ch15::{
-    CustomSmartPointer,
+    hello, CustomSmartPointer,
     List::{Cons, Nil},
     MyBox,
 };
@@ -13,9 +13,20 @@ fn main() {
     // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     // println!("{list:?}");
 
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating c = {}", Rc::strong_count(&a));
+
+    println!("{a:?}");
+    println!("{b:?}");
+    println!("{c:?}");
+
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // println!("count after c goes out of scope = {}", Rc::weak_count(&a));
 
     let x = 5;
     let y = &x;
@@ -39,4 +50,10 @@ fn main() {
     // c.drop();
     drop(c);
     println!("CustomSmartPointers created.");
+
+    let username = Box::new(String::from("choi"));
+    hello(&username);
+
+    let i = 4;
+    let j = &mut i;
 }
